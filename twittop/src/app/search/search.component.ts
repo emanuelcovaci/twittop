a52e731e47401cf79caf0441e3f30015bb4ec8b4
Emanuel Covaci <covaciemanuel98@gmail.com>
2020-06-27 01:45:12 +0300

    styles update

--- twittop/src/app/search/search.component.ts
+++ twittop/src/app/search/search.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -18,7 +18,16 @@ export class SearchComponent implements OnInit {
 
   onClickGo() {
     console.log(this.username);
-    this.route.navigate(['/twitter-profile/'+this.username, ]);
+    this.route.navigate(['/twitter-profile/' + this.username,]);
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (event.key == 'Enter') {
+      if (this.username != '') {
+        this.onClickGo();
+      }
+    }
   }
 
 }
